9896a1e8a88726f272a3be4ea4a0d010e7c40117
johnallec <63078606+johnallec@users.noreply.github.com>
2021-03-09 14:38:01 +0100

    Add files via upload

---
+++ build-ProgettoPO-2020-2021-Celia-215522-Desktop_Qt_5_15_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,203 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../ProgettoPO-2020-2021-Celia-215522/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[22];
+    char stringdata0[616];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 31), // "on_aggiungiAutoreButton_clicked"
+QT_MOC_LITERAL(2, 43, 0), // ""
+QT_MOC_LITERAL(3, 44, 35), // "on_aggiungiConferenzaButton_c..."
+QT_MOC_LITERAL(4, 80, 38), // "on_aggiungiOrganizzatoreButto..."
+QT_MOC_LITERAL(5, 119, 32), // "on_aggiungiRivistaButton_clicked"
+QT_MOC_LITERAL(6, 152, 33), // "on_aggiungiArticoloButton_cli..."
+QT_MOC_LITERAL(7, 186, 45), // "on_pubblicatoPerConferenzaRad..."
+QT_MOC_LITERAL(8, 232, 42), // "on_pubblicatoPerRivistaRadioB..."
+QT_MOC_LITERAL(9, 275, 32), // "on_aggiungiKeywordButton_clicked"
+QT_MOC_LITERAL(10, 308, 50), // "on_listaAutoriArticoloListWid..."
+QT_MOC_LITERAL(11, 359, 16), // "QListWidgetItem*"
+QT_MOC_LITERAL(12, 376, 4), // "item"
+QT_MOC_LITERAL(13, 381, 61), // "on_listaArticoliCorrelatiArti..."
+QT_MOC_LITERAL(14, 443, 22), // "on_b1c12Button_clicked"
+QT_MOC_LITERAL(15, 466, 20), // "on_b45Button_clicked"
+QT_MOC_LITERAL(16, 487, 24), // "on_b4RadioButton_clicked"
+QT_MOC_LITERAL(17, 512, 24), // "on_b5RadioButton_clicked"
+QT_MOC_LITERAL(18, 537, 19), // "on_c6Button_clicked"
+QT_MOC_LITERAL(19, 557, 18), // "on_dButton_clicked"
+QT_MOC_LITERAL(20, 576, 19), // "on_e6Button_clicked"
+QT_MOC_LITERAL(21, 596, 19) // "on_f4Button_clicked"
+
+    },
+    "MainWindow\0on_aggiungiAutoreButton_clicked\0"
+    "\0on_aggiungiConferenzaButton_clicked\0"
+    "on_aggiungiOrganizzatoreButton_clicked\0"
+    "on_aggiungiRivistaButton_clicked\0"
+    "on_aggiungiArticoloButton_clicked\0"
+    "on_pubblicatoPerConferenzaRadioButton_clicked\0"
+    "on_pubblicatoPerRivistaRadioButton_clicked\0"
+    "on_aggiungiKeywordButton_clicked\0"
+    "on_listaAutoriArticoloListWidget_itemDoubleClicked\0"
+    "QListWidgetItem*\0item\0"
+    "on_listaArticoliCorrelatiArticoloListWidget_itemDoubleClicked\0"
+    "on_b1c12Button_clicked\0on_b45Button_clicked\0"
+    "on_b4RadioButton_clicked\0"
+    "on_b5RadioButton_clicked\0on_c6Button_clicked\0"
+    "on_dButton_clicked\0on_e6Button_clicked\0"
+    "on_f4Button_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      18,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  104,    2, 0x08 /* Private */,
+       3,    0,  105,    2, 0x08 /* Private */,
+       4,    0,  106,    2, 0x08 /* Private */,
+       5,    0,  107,    2, 0x08 /* Private */,
+       6,    0,  108,    2, 0x08 /* Private */,
+       7,    0,  109,    2, 0x08 /* Private */,
+       8,    0,  110,    2, 0x08 /* Private */,
+       9,    0,  111,    2, 0x08 /* Private */,
+      10,    1,  112,    2, 0x08 /* Private */,
+      13,    1,  115,    2, 0x08 /* Private */,
+      14,    0,  118,    2, 0x08 /* Private */,
+      15,    0,  119,    2, 0x08 /* Private */,
+      16,    0,  120,    2, 0x08 /* Private */,
+      17,    0,  121,    2, 0x08 /* Private */,
+      18,    0,  122,    2, 0x08 /* Private */,
+      19,    0,  123,    2, 0x08 /* Private */,
+      20,    0,  124,    2, 0x08 /* Private */,
+      21,    0,  125,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 11,   12,
+    QMetaType::Void, 0x80000000 | 11,   12,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_aggiungiAutoreButton_clicked(); break;
+        case 1: _t->on_aggiungiConferenzaButton_clicked(); break;
+        case 2: _t->on_aggiungiOrganizzatoreButton_clicked(); break;
+        case 3: _t->on_aggiungiRivistaButton_clicked(); break;
+        case 4: _t->on_aggiungiArticoloButton_clicked(); break;
+        case 5: _t->on_pubblicatoPerConferenzaRadioButton_clicked(); break;
+        case 6: _t->on_pubblicatoPerRivistaRadioButton_clicked(); break;
+        case 7: _t->on_aggiungiKeywordButton_clicked(); break;
+        case 8: _t->on_listaAutoriArticoloListWidget_itemDoubleClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 9: _t->on_listaArticoliCorrelatiArticoloListWidget_itemDoubleClicked((*reinterpret_cast< QListWidgetItem*(*)>(_a[1]))); break;
+        case 10: _t->on_b1c12Button_clicked(); break;
+        case 11: _t->on_b45Button_clicked(); break;
+        case 12: _t->on_b4RadioButton_clicked(); break;
+        case 13: _t->on_b5RadioButton_clicked(); break;
+        case 14: _t->on_c6Button_clicked(); break;
+        case 15: _t->on_dButton_clicked(); break;
+        case 16: _t->on_e6Button_clicked(); break;
+        case 17: _t->on_f4Button_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 18)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 18;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 18)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 18;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
